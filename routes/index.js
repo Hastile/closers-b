18399261aca8b1ca8219f27c6149bf2956271234
Hastile <66550650+Hastile@users.noreply.github.com>
2023-03-20 09:20:53 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,44 @@ const express = require('express');
 const router = express.Router();
 
 
+///////
+const dayjs = require('dayjs');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+
+dayjs.extend(customParseFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+class DateUtil {
+  constructor() {
+    this.today = dayjs().tz('Asia/Seoul');
+    this.last4Hour = this.today.startOf('day').add(4, 'hour');
+    this.lastweek = this.today.set('day', 6).startOf('day').add(4, 'hour');
+  }
+
+  getToday() {
+    return this.today.format('YYYY-MM-DD HH:mm:ss');
+  }
+
+  getLast4Hour() {
+    const last4Hour = this.today.startOf('day').add(4, 'hour');
+    return (this.today.get('hour') < 4 ? last4Hour.subtract(1, 'day') : last4Hour).format('YYYY-MM-DD HH:mm:ss');
+  }
+
+  getLastWeek() {
+    const lastWeek = this.today.set('day', 6).startOf('day').add(4, 'hour');
+    return (this.today.get('day') < 6 ? lastWeek.subtract(1, 'week') : this.today.get('hour') < 4 ? lastWeek.subtract(1, 'week') : lastWeek).format('YYYY-MM-DD HH:mm:ss');
+  }
+}
+//////
+
 const { closersdb } = require('./mysql');
 
 /* GET home page. */
 router.post('/', async (req, res) => {
-  const dateUtil = require('./time');
+  const dateUtil = new DateUtil()
   console.log(req.body.accountName)
   const data = await closersdb(`SELECT * FROM characters`)
   // res.send(data)
@@ -57,7 +90,7 @@ router.post('/db/check/write', async (req, res) => {
 });
 
 router.post('/db/check/raid', async (req, res) => {
-  const dateUtil = require('./time');
+  const dateUtil = new DateUtil()
   const { name } = req.body
   const row = await closersdb(`SELECT * FROM record WHERE \`character\` = '${name}' AND dungeon IN (SELECT name FROM dungeons WHERE is_raid = 1)`)
   const count = [row.filter(r => r.dungeon == '기계왕 벨페고르'), row.filter(r => r.dungeon == '야수왕 베히모스')]
